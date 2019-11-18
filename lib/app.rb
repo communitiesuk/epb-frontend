c30f4aa829927a729a7069f271209ff414b42fa2
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-11-18 17:20:20 +0000

    Make the app aware of i18n

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,24 @@
 require 'sinatra/base'
 
+require 'i18n'
+require 'i18n/backend/fallbacks'
+
+require 'rack/contrib'
+
 class FrontendService < Sinatra::Base
   set :public_folder, Proc.new { File.join(root, "/../public") }
+
+  use Rack::Locale
+  I18n.load_path = Dir[File.join(settings.root, '/../locales', '*.yml')]
+  I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+  I18n.backend.load_translations
+
+  I18n.enforce_available_locales = false
+  I18n.available_locales = [:cy, 'en-GB']
+
+  def t(*args)
+    I18n.t(*args)
+  end
 
   get '/' do
     erb :index
