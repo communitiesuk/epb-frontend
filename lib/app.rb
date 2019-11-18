c5c914e54654dbeba6bda49840c3528f0c06e1bc
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2019-11-18 09:43:36 +0000

    Lawrence: clean up robocop offences

--- lib/app.rb
+++ lib/app.rb
@@ -1,7 +1,9 @@
 require 'sinatra/base'
 
 class FrontendService < Sinatra::Base
-  set :public_folder, Proc.new { File.join(root, "/../public") }
+  set :public_folder, proc do
+    File.join(root, '/../public')
+  end
 
   get '/' do
     erb :index
