6e83a3b6cffaf77e6a70e6c618c3f7e536002d10
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-11-19 13:35:47 +0000

    Marten & Jaseera: Add test for locales in layout

--- spec/acceptance/locales_spec.rb
+++ spec/acceptance/locales_spec.rb
@@ -1,21 +1,29 @@
 require 'app'
 
-describe FrontendService do
-  describe 'the server displaying html' do
-    context 'compare the text on index' do
-      let(:response) { get '/' }
+  describe FrontendService do
+    describe 'the server displaying html' do
+      context 'compare the text on index' do
+        let(:response) { get '/' }
 
-      it 'does display the page title' do
-        expect(response.body).to include('Find an energy assessor for a residential property')
+        it 'does display the page title' do
+          expect(response.body).to include('Find an energy assessor for a residential property')
+        end
       end
-    end
 
-    context 'compare the text on schemes' do
-      let(:response) { get '/schemes' }
+      context 'compare the text on schemes' do
+        let(:response) { get '/schemes' }
 
-      it 'does display the page title' do
-        expect(response.body).to include('Contact accreditation schemes for energy assessors')
+        it 'does display the page title' do
+          expect(response.body).to include('Contact accreditation schemes for energy assessors')
+        end
+      end
+
+      context 'compare the text in head' do
+        let(:response) { get '/' }
+
+        it 'does display the title with tags around it' do
+          expect(response.body).to include('<title>Energy performance of buildings register</title>')
+        end
       end
     end
   end
-end
