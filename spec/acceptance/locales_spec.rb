01adb543fada6f02d311cb5712183cfb8bb6ecb5
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-11-19 12:12:29 +0000

    Add test for english locales in /schemes

--- spec/acceptance/locales_spec.rb
+++ spec/acceptance/locales_spec.rb
@@ -1,12 +1,20 @@
 require 'app'
 
 describe FrontendService do
-  describe 'the server displaying the index page' do
-    context 'compare the text' do
+  describe 'the server displaying html' do
+    context 'compare the text on index' do
       let(:response) { get '/' }
 
       it 'does display the page title' do
         expect(response.body).to include('Find an energy assessor for a residential property')
+      end
+    end
+
+    context 'compare the text on schemes' do
+      let(:response) { get '/schemes' }
+
+      it 'does display the page title' do
+        expect(response.body).to include('Contact accreditation schemes for energy assessors')
       end
     end
   end
