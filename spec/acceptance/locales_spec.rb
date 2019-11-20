7345a6c5a24c94e9d899f3832166269523e189ae
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2019-11-20 09:49:33 +0000

    Add tests for locales switching

--- spec/acceptance/locales_spec.rb
+++ spec/acceptance/locales_spec.rb
@@ -1,28 +1,41 @@
 require 'app'
 
   describe FrontendService do
-    describe 'the server displaying html' do
-      context 'compare the text on index' do
+    describe 'Given the server displaying html' do
+      context 'When we compare the text on index' do
         let(:response) { get '/' }
 
-        it 'does display the page title' do
+        it 'Then it does display the page title' do
           expect(response.body).to include('Find an energy assessor for a residential property')
         end
       end
 
-      context 'compare the text on schemes' do
+      context 'When we compare the text on schemes' do
         let(:response) { get '/schemes' }
 
-        it 'does display the page title' do
+        it 'Then it does display the page title' do
           expect(response.body).to include('Contact accreditation schemes for energy assessors')
         end
       end
 
-      context 'compare the text in head' do
+      context 'When we compare the text in head' do
         let(:response) { get '/' }
 
-        it 'does display the title with tags around it' do
+        it 'Then it does display the title with tags around it' do
           expect(response.body).to include('<title>Energy performance of buildings register</title>')
+        end
+      end
+
+      context 'When we switch languages' do
+        let(:response_welsh) { get '/?lang=cy' }
+        let(:response_english) { get '/?lang=en' }
+
+        it 'Then it does show Welsh: on cy' do
+          expect(response_welsh.body).to include('Welsh:')
+        end
+
+        it 'Then it does not show Welsh: on en' do
+          expect(response_english.body).not_to include('Welsh:')
         end
       end
     end
