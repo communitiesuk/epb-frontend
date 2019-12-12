95991f8dacab56efa4851294f6d5522513c26197
Kev Keenoy <kevin.keenoy@cabinetoffice.gov.uk>
2019-12-12 18:23:15 +0000

    Rename some rspec tests following betterspecs.org guidance
    See http://www.betterspecs.org/
    
    These tweaks make the names printed for failed test output make a bit more sense:
    e.g. The test that previously printed:
    "FrontendService the server having started responses from / renders the home page"
    Is now:
    "FrontendService running .get / renders the home page"

--- spec/acceptance/server_spec.rb
+++ spec/acceptance/server_spec.rb
@@ -1,30 +1,30 @@
 require 'app'
 
-describe FrontendService do
-  describe 'the server having started' do
-    context 'responses from /' do
-      let(:response) { get '/' }
+describe FrontendService, "running" do
 
-      it 'renders the home page' do
-        expect(response.status).to eq(200)
-        expect(response.body).to include('Energy performance of buildings')
-      end
+  describe '.get /' do
+    let(:response) { get '/' }
+
+    it 'renders the home page' do
+      expect(response.status).to eq(201)
+      expect(response.body).to include('Energy performance of buildings')
     end
+  end
 
-    context 'passes a healthcheck' do
-      let(:response) { get '/healthcheck' }
+  describe '.get /healthcheck' do
+    let(:response) { get '/healthcheck' }
 
-      it 'returns status 200' do
-        expect(response.status).to eq(200)
-      end
+    it 'returns status 200' do
+      expect(response.status).to eq(200)
     end
+  end
 
-    context 'errors on a non-existent page' do
-      let(:response) { get '/this-page-does-not-exist' }
+  describe '.get a non-existent page' do
+    let(:response) { get '/this-page-does-not-exist' }
 
-      it 'returns status 404' do
-        expect(response.status).to eq(404)
-      end
+    it 'returns status 404' do
+      expect(response.status).to eq(404)
     end
   end
 end
+
--- spec/integration/rackup_spec.rb
+++ spec/integration/rackup_spec.rb
@@ -1,37 +1,40 @@
 require 'net/http'
 
-describe 'starts server outside of ruby' do
-  describe 'the server running live' do
-    before(:all) do
-      process = IO.popen('rackup -q')
-      @process_id = process.pid
+describe 'Running behind Rack server' do
+  before(:all) do
+    puts '' # Add newline to test output before WEBrick log output
+    process = IO.popen('rackup -q')
+    @process_id = process.pid
 
-      sleep 2
+    sleep 2
+  end
+
+  after(:all) { Process.kill('KILL', @process_id) }
+
+  let(:request) { Net::HTTP.new('localhost', 9_292) }
+
+  describe 'GET /' do
+    it 'renders the home page' do
+      req = Net::HTTP::Get.new('/')
+      response = request.request(req)
+      expect(response.code).to eq('200')
+      expect(response.body).to include('Energy performance of buildings')
     end
+  end
 
-    after(:all) { Process.kill('KILL', @process_id) }
+  describe 'GET /healthcheck' do
+    it 'passes a healthcheck' do
+      req = Net::HTTP::Get.new('/healthcheck')
+      response = request.request(req)
+      expect(response.code).to eq('200')
+    end
+  end
 
-    let(:request) { Net::HTTP.new('localhost', 9_292) }
-
-    context 'when the server is up' do
-      it 'renders the home page' do
-        req = Net::HTTP::Get.new('/')
-        response = request.request(req)
-        expect(response.code).to eq('200')
-        expect(response.body).to include('Energy performance of buildings')
-      end
-
-      it 'passes a healthcheck' do
-        req = Net::HTTP::Get.new('/healthcheck')
-        response = request.request(req)
-        expect(response.code).to eq('200')
-      end
-
-      it 'errors on a non-existent page' do
-        req = Net::HTTP::Get.new('/this-page-does-not-exist')
-        response = request.request(req)
-        expect(response.code).to eq('404')
-      end
+  describe 'GET non-existent page' do
+    it 'returns status 404' do
+      req = Net::HTTP::Get.new('/this-page-does-not-exist')
+      response = request.request(req)
+      expect(response.code).to eq('404')
     end
   end
 end
