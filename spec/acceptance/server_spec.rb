711013f0c3831e8041611b77996dcc80c373fc02
Kev Keenoy <kevin.keenoy@cabinetoffice.gov.uk>
2019-12-12 18:40:21 +0000

    Move some tests and remove duplicate/unnecessary tests
    This also renames some rspec tests following betterspecs.org guidance
    See http://www.betterspecs.org/

--- spec/acceptance/server_spec.rb
+++ spec/acceptance/server_spec.rb
@@ -5,9 +5,14 @@ describe FrontendService, "running" do
   describe '.get /' do
     let(:response) { get '/' }
 
-    it 'renders the home page' do
-      expect(response.status).to eq(201)
-      expect(response.body).to include('Energy performance of buildings')
+    it 'returns status 200' do
+      expect(response.status).to eq(200)
+    end
+    it 'includes the home page title' do
+      expect(response.body).to include('<title>Energy performance of buildings register</title>')
+    end
+    it 'displays the home page heading' do
+      expect(response.body).to include('Find an energy assessor for a residential property')
     end
   end
 
@@ -16,6 +21,14 @@ describe FrontendService, "running" do
 
     it 'returns status 200' do
       expect(response.status).to eq(200)
+    end
+  end
+
+  describe '.get /schemes' do
+    let(:response) { get '/schemes' }
+
+    it 'displays the schemes page title' do
+      expect(response.body).to include('Contact an energy assessor accreditation scheme')
     end
   end
 
