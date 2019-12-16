2cb63b3647977d481b9c0fc47a532e45cf2c2a6c
Yusuf <yusuf@madetech.com>
2019-12-16 15:15:21 +0000

    Implement Jest for finding an assessor by postcode

--- spec/journey/find_assessor_by_postcode.test.js
+++ spec/journey/find_assessor_by_postcode.test.js
@@ -1,15 +1,18 @@
 const { openBrowser, goto, write, into, click, closeBrowser, textBox } = require('taiko');
 
-(async () => {
-  try {
+describe('Finding an assessor by postcode', () => {
+  beforeAll(async () => {
     await openBrowser();
+  });
+
+  it('finds an assessor by postcode', async () => {
     await goto("localhost:9292");
     await click("Start now");
-    await write('SW1A 2AA', into(textBox( 'postcode')))
-    await click('Find')
-  } catch (error) {
-    console.error(error);
-  } finally {
+    await write('SW1A 2AA', into(textBox('postcode')));
+    await click('Find');
+  }, 30000);
+
+  afterAll(async () => {
     await closeBrowser();
-  }
-})();
+  });
+});
