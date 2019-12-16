1728e59819709bc41dd7668c7fa1c02c2734d57c
Yusuf <yusuf@madetech.com>
2019-12-16 17:10:39 +0000

    Set up HTTP server within journey test and kill it afterwards
    Co-authored-by: Christine <splogy2001@hotmail.com>

--- spec/journey/find_assessor_by_postcode.test.js
+++ spec/journey/find_assessor_by_postcode.test.js
@@ -1,8 +1,16 @@
 const { openBrowser, goto, write, into, click, closeBrowser, textBox } = require('taiko');
+const { spawn } = require('child_process');
+const http = require('http');
 
 describe('Finding an assessor by postcode', () => {
+  let process_id;
+  let server;
+
   beforeAll(async () => {
-    await openBrowser();
+    process_id = spawn('rackup', ['-q']);
+    server = await http.createServer(function (req, res) {
+    }).listen(9292);
+    await openBrowser({ headless: false });
   });
 
   it('finds an assessor by postcode', async () => {
@@ -12,7 +20,9 @@ describe('Finding an assessor by postcode', () => {
     await click('Find');
   }, 30000);
 
-  afterAll(async () => {
+  afterAll(async (done) => {
     await closeBrowser();
+    await server.close(done);
+    process.kill(process_id.pid, "SIGTERM")
   });
 });
