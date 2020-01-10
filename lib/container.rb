4eeea825f5fde8c241c5d3372b868c06255e2396
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2020-01-10 10:44:30 +0000

    Add container class

---
+++ lib/container.rb
@@ -0,0 +1,23 @@
+class Container
+  def initialize(oauth_client = OAuth2::Client)
+    internal_api_client =
+      Auth::HttpClient.new ENV['AUTH_CLIENT_ID'],
+                           ENV['AUTH_CLIENT_SECRET'],
+                           ENV['AUTH_SERVER'],
+                           ENV['EPB_API_URL'],
+                           oauth_client
+
+    assessors_gateway = Gateway::AssessorsGateway.new(internal_api_client)
+
+    find_assessor_use_case = UseCase::FindAssessor.new(assessors_gateway)
+
+    @objects = {
+      internal_api_client: internal_api_client,
+      find_assessor_use_case: find_assessor_use_case
+    }
+  end
+
+  def get_object(key)
+    @objects[key]
+  end
+end
