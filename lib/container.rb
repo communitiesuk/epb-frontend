a1c194989dc6f7db9a3c6afe4e4cd06823ccd799
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2020-01-14 15:15:12 +0000

    Remove redudant args from container

--- lib/container.rb
+++ lib/container.rb
@@ -1,16 +1,12 @@
 class Container
-  def initialize(internal_api_client = nil)
-    unless internal_api_client
-      internal_api_client =
-        Auth::HttpClient.new ENV['EPB_AUTH_CLIENT_ID'],
+  def initialize
+      internal_api_client = Auth::HttpClient.new ENV['EPB_AUTH_CLIENT_ID'],
                              ENV['EPB_AUTH_CLIENT_SECRET'],
                              ENV['EPB_AUTH_SERVER'],
                              ENV['EPB_API_URL'],
                              OAuth2::Client
-    end
 
     assessors_gateway = Gateway::AssessorsGateway.new(internal_api_client)
-
     find_assessor_use_case = UseCase::FindAssessor.new(assessors_gateway)
 
     @objects = {
