8002a4ed2c2a890a56c63c6b3ac227a9a1411141
Tito Sarrionandia <tito@madetech.com>
2020-01-14 16:45:13 +0000

    Add case for when certificate doesnt exist

--- lib/gateway/energy_assessments_gateway.rb
+++ lib/gateway/energy_assessments_gateway.rb
@@ -5,8 +5,13 @@ module Gateway
     end
 
     def fetch_assessment(assessment_id)
-      route = URI.encode("api/assessments/domestic-energy-performance/#{assessment_id}")
-      nil
+      route = URI.encode("/api/assessments/domestic-energy-performance/#{assessment_id}")
+      begin
+        response = @internal_api_client.get(route)
+        JSON.parse(response.body, symbolize_names: true)
+      rescue Exception => e
+        nil
+      end
     end
   end
 end
--- spec/unit/gateway/energy_assessments_gateway_spec.rb
+++ spec/unit/gateway/energy_assessments_gateway_spec.rb
@@ -16,9 +16,24 @@ describe Gateway::EnergyAssessmentsGateway do
               status: 404
           )
     end
+
     it 'returns nil' do
       result = gateway.fetch_assessment('123-456')
       expect(result).to be_nil
+    end
+  end
+
+  context 'when a certificate does exist' do
+    before do
+      stub_request(:get, 'http://test-api.gov.uk/api/assessments/domestic-energy-performance/122-456')
+          .to_return(
+              status: 200,
+              body: {}.to_json
+          )
+    end
+    it 'returns assessments' do
+      result = gateway.fetch_assessment('122-456')
+      expect(result).to eq({})
     end
   end
 end
