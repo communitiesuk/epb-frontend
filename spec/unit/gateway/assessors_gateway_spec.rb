96fcfc064642ac10775620bd289fa850c5e836f2
Jaseera Abubacker <j.p.mohamedabubacker@gmail.com>
2020-01-15 17:05:21 +0000

    Add gateway test for error codes

--- spec/unit/gateway/assessors_gateway_spec.rb
+++ spec/unit/gateway/assessors_gateway_spec.rb
@@ -99,4 +99,100 @@ describe Gateway::AssessorsGateway do
       expect(response).to eq({ results: [], searchPostcode: 'BF1 3AA' })
     end
   end
+
+  context 'when the postcode is not registered' do
+    let(:response) { gateway.search('AF1+3AA') }
+
+    before do
+      stub_request(:get, 'http://test-api.gov.uk/api/assessors/search/AF1+3AA')
+        .to_return(
+        status: 200,
+        body: {
+          "errors": [
+            {
+              "code": 'NOT_FOUND',
+              "message": 'The requested postcode is not registered'
+            }
+          ]
+        }.to_json
+      )
+    end
+
+    it 'returns not found error response' do
+      expect(response).to eq(
+        {
+          "errors": [
+            {
+              "code": 'NOT_FOUND',
+              "message": 'The requested postcode is not registered'
+            }
+          ]
+        }
+      )
+    end
+  end
+
+  context 'when the postcode is not valid' do
+    let(:response) { gateway.search('1+3AA') }
+
+    before do
+      stub_request(:get, 'http://test-api.gov.uk/api/assessors/search/1+3AA')
+        .to_return(
+        status: 200,
+        body: {
+          "errors": [
+            {
+              "code": 'INVALID_REQUEST',
+              "title": 'The requested postcode is not valid'
+            }
+          ]
+        }.to_json
+      )
+    end
+
+    it 'returns invalid request error' do
+      expect(response).to eq(
+        {
+          "errors": [
+            {
+              "code": 'INVALID_REQUEST',
+              "title": 'The requested postcode is not valid'
+            }
+          ]
+        }
+      )
+    end
+  end
+
+  context 'when there is no scheme' do
+    let(:response) { gateway.search('1+3AA') }
+
+    before do
+      stub_request(:get, 'http://test-api.gov.uk/api/assessors/search/1+3AA')
+        .to_return(
+        status: 200,
+        body: {
+          "errors": [
+            {
+              "code": 'SCHEME_NOT_FOUND',
+              "message": 'There is no scheme for one of the requested assessor'
+            }
+          ]
+        }.to_json
+      )
+    end
+
+    it 'returns invalid request error' do
+      expect(response).to eq(
+        {
+          "errors": [
+            {
+              "code": 'SCHEME_NOT_FOUND',
+              "message": 'There is no scheme for one of the requested assessor'
+            }
+          ]
+        }
+      )
+    end
+  end
 end
