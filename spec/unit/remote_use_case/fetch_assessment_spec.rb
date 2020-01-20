38f21531ae02e2953bff3904dc597ffbe07c5da7
Yusuf <yusuf@madetech.com>
2020-01-20 16:56:44 +0000

    Refactor: Rename gateway in remote use case spec file to reflect remote use case for fetching an assessment
    Co-authored-by: Kev <kevin.keenoy@communities.gov.uk>

--- spec/unit/remote_use_case/fetch_assessment_spec.rb
+++ spec/unit/remote_use_case/fetch_assessment_spec.rb
@@ -3,7 +3,7 @@
 describe RemoteUseCase::FetchAssessment do
   include RSpecUnitMixin
 
-  let(:gateway) do
+  let(:fetch_assessment) do
     described_class.new(container.get_object(:internal_api_client))
   end
 
@@ -11,7 +11,7 @@ describe RemoteUseCase::FetchAssessment do
     before { FetchAssessmentNoAssessmentStub.fetch }
 
     it 'raises an AssessmentNotFound error' do
-      expect { gateway.execute('123-456') }.to raise_error(
+      expect { fetch_assessment.execute('123-456') }.to raise_error(
         RemoteUseCase::FetchAssessment::AssessmentNotFound
       )
     end
@@ -20,17 +20,15 @@ describe RemoteUseCase::FetchAssessment do
   context 'when an assessment does exist' do
     before { FetchAssessmentStub.fetch('122-456') }
     it 'returns assessments' do
-      result = gateway.execute('122-456')
+      result = fetch_assessment.execute('122-456')
       expect(result).to eq(
-        {
-          address_summary: '2 Marsham Street, London, SW1B 2BB',
-          assessment_id: '123-456',
-          date_of_assessment: '02 January 2020',
-          date_registered: '05 January 2020',
-          dwelling_type: 'Top floor flat',
-          total_floor_area: 150,
-          type_of_assessment: 'RdSAP'
-        }
+        address_summary: '2 Marsham Street, London, SW1B 2BB',
+        assessment_id: '123-456',
+        date_of_assessment: '02 January 2020',
+        date_registered: '05 January 2020',
+        dwelling_type: 'Top floor flat',
+        total_floor_area: 150,
+        type_of_assessment: 'RdSAP'
       )
     end
   end
