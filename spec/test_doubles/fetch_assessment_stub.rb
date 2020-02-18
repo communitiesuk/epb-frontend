18727e1322fb86c4209a8a507e062039c7f992c0
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-02-18 13:27:44 +0000

    Update stub with another assessment, date of expiry and postcode
    Co-authored-by: Yusuf <yusuf@madetech.com>

--- spec/test_doubles/fetch_assessment_stub.rb
+++ spec/test_doubles/fetch_assessment_stub.rb
@@ -2,6 +2,40 @@
 
 class FetchAssessmentStub
   def self.fetch(assessment_id)
+    if assessment_id == '123-456'
+      body = {
+        addressSummary: '2 Marsham Street, London, SW1B 2BB',
+        assessmentId: '123-456',
+        dateRegistered: '2020-01-05',
+        dateOfExpiry: '2030-01-05',
+        dateOfAssessment: '2020-01-02',
+        dwellingType: 'Top floor flat',
+        typeOfAssessment: 'RdSAP',
+        totalFloorArea: 150,
+        currentEnergyEfficiencyRating: 90,
+        currentEnergyEfficiencyBand: 'b',
+        potentialEnergyEfficiencyRating: 99,
+        potentialEnergyEfficiencyBand: 'a',
+        postcode: 'SW1B 2BB'
+      }
+    else
+      body = {
+        addressSummary: '2 Marsham Street, London, SW1B 2BB',
+        assessmentId: '223-456',
+        dateRegistered: '2020-01-05',
+        dateOfExpiry: '2030-01-05',
+        dateOfAssessment: '2020-01-02',
+        dwellingType: 'Top floor flat',
+        typeOfAssessment: 'RdSAP',
+        totalFloorArea: 150,
+        currentEnergyEfficiencyRating: 25,
+        currentEnergyEfficiencyBand: 'f',
+        potentialEnergyEfficiencyRating: 99,
+        potentialEnergyEfficiencyBand: 'a',
+        postcode: 'SW1B 2BB'
+      }
+    end
+
     WebMock.stub_request(
       :get,
       "http://test-api.gov.uk/api/assessments/domestic-energy-performance/#{
@@ -10,19 +44,7 @@ class FetchAssessmentStub
     )
       .to_return(
       status: 200,
-      body: {
-        addressSummary: '2 Marsham Street, London, SW1B 2BB',
-        assessmentId: '123-456',
-        dateRegistered: '2020-01-05',
-        "dateOfAssessment": '2020-01-02',
-        "dwellingType": 'Top floor flat',
-        "typeOfAssessment": 'RdSAP',
-        "totalFloorArea": 150,
-        currentEnergyEfficiencyRating: 95,
-        currentEnergyEfficiencyBand: 'a',
-        potentialEnergyEfficiencyRating: 95,
-        potentialEnergyEfficiencyBand: 'b'
-      }.to_json
+      body: body.to_json
     )
   end
 end
