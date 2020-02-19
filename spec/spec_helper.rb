caf9f936697b999e79b4915c56de5135078f59d4
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-02-19 16:38:35 +0000

    Build folder structure for stubbed responses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,8 @@ require 'zeitwerk'
 loader = Zeitwerk::Loader.new
 loader.push_dir("#{__dir__}/../lib/")
 loader.push_dir("#{__dir__}/../spec/test_doubles/")
+loader.push_dir("#{__dir__}/../spec/test_doubles/find_assessor/by_name")
+loader.push_dir("#{__dir__}/../spec/test_doubles/find_assessor/by_postcode")
 loader.setup
 
 AUTH_URL = 'http://test-auth-server.gov.uk'
