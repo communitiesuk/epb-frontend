2f093545a6223f24786597cb605b70cb829a29de
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-02-25 18:40:29 +0000

    Add use case for find assessor by reference number

---
+++ lib/use_case/find_certificate_by_reference_number.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module UseCase
+  class FindCertificateByReferenceNumber
+    class ReferenceNumberNotValid < RuntimeError; end
+    class AuthTokenMissing < RuntimeError; end
+
+    def initialize(assessors_gateway)
+      @certificates_gateway = assessors_gateway
+    end
+
+    def execute(query)
+      gateway_response = @certificates_gateway.search(query)
+
+      if gateway_response.include?(:errors)
+        gateway_response[:errors].each do |error|
+          raise ReferenceNumberNotValid if error[:code] == 'INVALID_REQUEST'
+          raise AuthTokenMissing if error[:code] == 'Auth::Errors::TokenMissing'
+        end
+      end
+
+      gateway_response[:results]
+    end
+  end
+end
