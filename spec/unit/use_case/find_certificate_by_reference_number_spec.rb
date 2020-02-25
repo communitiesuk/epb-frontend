21aa1ed96c0a337d27f16668e379c733ff978176
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-02-25 18:40:49 +0000

    Add unit tests for use case

---
+++ spec/unit/use_case/find_certificate_by_reference_number_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+describe UseCase::FindCertificateByReferenceNumber do
+  context 'when there are no certificates by that reference number' do
+    let(:certificates_gateway) { CertificatesGatewayEmptyStub.new }
+    let(:find_certificate) { described_class.new(certificates_gateway) }
+
+    it 'returns empty array' do
+      expect(find_certificate.execute('1234-5678-9101-1121')).to eq([])
+    end
+  end
+
+  context 'when there are certificates with that reference number' do
+    let(:valid_certificates) do
+      [
+        {
+          assessmentId: '123-456',
+          dateOfAssessment: '2011-01-01',
+          dateRegistered: '2011-01-02',
+          dwellingType: 'Top floor flat',
+          typeOfAssessment: 'RdSAP',
+          totalFloorArea: 50,
+          addressSummary: '2 Marsham Street, London, SW1B 2BB',
+          currentEnergyEfficiencyRating: 90,
+          currentEnergyEfficiencyBand: 'b',
+          potentialEnergyEfficiencyRating: 'a',
+          potentialEnergyEfficiencyBand: 95,
+          postcode: 'SW1B 2BB',
+          dateOfExpiry: '2021-01-01'
+        },
+        {
+          assessmentId: '123-987',
+          dateOfAssessment: '2011-01-01',
+          dateRegistered: '2011-01-02',
+          dwellingType: 'Top floor flat',
+          typeOfAssessment: 'RdSAP',
+          totalFloorArea: 50,
+          addressSummary: '1 Marsham Street, London, SW1B 2BB',
+          currentEnergyEfficiencyRating: 90,
+          currentEnergyEfficiencyBand: 'b',
+          potentialEnergyEfficiencyRating: 'a',
+          potentialEnergyEfficiencyBand: 95,
+          postcode: 'SW1B 2BB',
+          dateOfExpiry: '2022-01-01'
+        },
+        {
+          assessmentId: '123-456',
+          dateOfAssessment: '2011-01-01',
+          dateRegistered: '2011-01-02',
+          dwellingType: 'Top floor flat',
+          typeOfAssessment: 'RdSAP',
+          totalFloorArea: 50,
+          addressSummary: '3 Marsham Street, London, SW1B 2BB',
+          currentEnergyEfficiencyRating: 90,
+          currentEnergyEfficiencyBand: 'b',
+          potentialEnergyEfficiencyRating: 'a',
+          potentialEnergyEfficiencyBand: 95,
+          postcode: 'SW1B 2BB',
+          dateOfExpiry: '2023-01-01'
+        }
+      ]
+    end
+
+    let(:certificates_gateway) { CertificatesGatewayStub.new }
+    let(:find_certificate) { described_class.new(certificates_gateway) }
+
+    it 'returns list of certificates' do
+      expect(find_certificate.execute('1234-5678-9101-1121')).to eq(valid_certificates)
+    end
+  end
+end
