0719358c1b36b73a51adde6be56020e3370807b0
Yusuf <yusuf@madetech.com>
2020-03-09 14:01:08 +0000

    Refactor: Use guard clause instead of nested if block

--- lib/use_case/base.rb
+++ lib/use_case/base.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module UseCase
   class Base
     def initialize(gateway)
@@ -5,13 +7,14 @@ module UseCase
     end
 
     def raise_errors_if_exists(response)
-      if response.include?(:errors)
-        response[:errors].each do |error|
-          if error[:code] == 'Auth::Errors::TokenMissing'
-            raise Errors::AuthTokenMissing
-          end
-          yield(error[:code])
+      return unless response.include?(:errors)
+
+      response[:errors].each do |error|
+        if error[:code] == 'Auth::Errors::TokenMissing'
+          raise Errors::AuthTokenMissing
         end
+
+        yield(error[:code])
       end
     end
   end
