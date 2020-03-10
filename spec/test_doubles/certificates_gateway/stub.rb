02dd67b8d72312d145ae2e81c66f1901cfa93448
Yusuf <yusuf@madetech.com>
2020-03-10 09:49:31 +0000

    Add assessor object to certificates gateway stub to reflect API response for fetch certificate use case

--- spec/test_doubles/certificates_gateway/stub.rb
+++ spec/test_doubles/certificates_gateway/stub.rb
@@ -139,6 +139,24 @@ module CertificatesGateway
 
     def fetch(assessment_id)
       {
+        assessor: {
+          firstName: 'Test',
+          lastName: 'Boi',
+          registeredBy: {
+            name: 'Quidos',
+            schemeId: 1
+          },
+          schemeAssessorId: 'TESTASSESSOR',
+          dateOfBirth: '2019-12-04',
+          contactDetails: {
+            telephoneNumber: '12345678901',
+            email: 'test.boi@quidos.com'
+          },
+          searchResultsComparisonPostcode: 'SW1A 2AA',
+          qualifications: {
+            domesticRdSap: 'ACTIVE'
+          }
+        },
         addressSummary: '2 Marsham Street, London, SW1B 2BB',
         assessmentId: assessment_id,
         dateOfAssessment: '02 January 2020',
