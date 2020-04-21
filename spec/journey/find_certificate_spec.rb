3216ac379d0d997bbd8b895986c3b94e24f917ef
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2020-04-21 10:38:24 +0200

    Add journey tests for Capybara in find certificate

---
+++ spec/journey/find_certificate_spec.rb
@@ -0,0 +1,126 @@
+describe 'Journey::FindCertificate', type: :feature, journey: true do
+  before(:all) do
+    process =
+      IO.popen(
+        ['rackup', 'config_test.ru', '-q', '-o', '127.0.0.1', '-p', '9393', err: %i[child out]]
+      )
+    @process_id = process.pid
+
+    unless process.readline.include?('port=9393')
+    end
+  end
+
+  after(:all) { Process.kill('KILL', @process_id) }
+
+  it 'finds a certificate by postcode' do
+    visit '/find-a-certificate'
+    click_on 'Start now'
+    fill_in 'postcode', with: 'SW1A 2AA'
+    click_button 'Find'
+    expect(page).to have_content '1-3 of 3 results matching SW1A 2AA'
+  end
+
+  it 'finds a certificate by postcode in Welsh' do
+    visit "/find-a-certificate"
+    click_on 'Welsh (Cymraeg)'
+    click_on 'Welsh: Start now'
+    fill_in 'postcode', with: 'SW1A 2AA'
+    click_button 'Welsh: Find'
+    expect(page).to have_content 'Welsh: 1-3 of 3 results matching SW1A 2AA'
+  end
+
+  it 'displays an error message when entering an empty postcode' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    fill_in 'postcode', with: ''
+    click_on('Find')
+    expect(page).to have_content 'Enter a real postcode'
+  end
+
+  it 'displays an error message when entering an invalid postcode' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    fill_in 'postcode', with: 'NOT A POSTCODE'
+    click_on 'Find'
+    expect(page).to have_content 'Enter a real postcode'
+  end
+
+
+  it 'displays the find a certificate page heading when entering a valid postcode' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    fill_in 'postcode', with: 'SW1A 2AA'
+    click_on 'Find'
+    expect(page).to have_content 'of 3 results matching'
+  end
+
+
+  it 'displays an error message when entering an empty reference number' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    click_on 'certificate by reference'
+    fill_in 'reference_number', with: ''
+    click_on 'Find'
+    expect(page).to have_content 'Enter a valid reference number'
+  end
+
+  it 'displays the find a certificate page heading when entering a valid certificate reference number' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    click_on 'certificate by reference'
+    fill_in 'reference_number', with: '1234-5678-9101-1121'
+    click_on 'Find'
+    expect(page).to have_content 'of 1 results matching'
+  end
+
+
+  it 'displays an error message when entering an empty street name' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    click_on 'Find certificate by street name'
+    fill_in 'town', with: 'Beauty Town'
+    click_on 'Find'
+    expect(page).to have_content 'Enter a street name'
+  end
+
+  it 'displays an error message when entering an empty town' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    click_on 'Find certificate by street name'
+    fill_in 'street_name', with: '1 Makeup Street'
+    click_on 'Find'
+    expect(page).to have_content 'Enter a town'
+  end
+
+  it 'displays an error message when entering an empty town and street name' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    click_on 'Find certificate by street name'
+    click_on 'Find'
+    expect(page).to have_content 'Enter a town'
+    expect(page).to have_content 'Enter a street name'
+  end
+
+  it 'displays the find a certificate page heading when entering a valid query' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    click_on 'Find certificate by street name'
+    fill_in 'street_name', with: '1 Makeup Street'
+    fill_in 'town', with: 'Beauty Town'
+    click_on 'Find'
+    expect(page).to have_content 'of 3 results matching'
+  end
+
+  it 'displays the error message when searching for a certificate that doesnt exist' do
+    visit "/find-a-certificate"
+    click_on "Start now"
+    click_on 'Find certificate by street name'
+    fill_in 'street_name', with: 'Madeup Street'
+    fill_in 'town', with: 'Madeup Town'
+    click_on 'Find'
+    expect(page).to have_content 'A certificate was not found at this address.'
+    click_on 'Find an assessor'
+
+    expect(page).to have_content 'Getting a new Energy Performance Certificate (EPC)'
+  end
+end
