ded5cedf475ba69f39f4a122384aeb98ec2bf84e
Yusuf <yusuf@madetech.com>
2020-06-25 15:06:14 +0100

    Add energy rating title and current energy efficiency band to non-domestic energy performance certificate
    Co-authored-by: Christine <splogy2001@hotmail.com>

--- spec/acceptance/view_non_domestic_spec.rb
+++ spec/acceptance/view_non_domestic_spec.rb
@@ -10,7 +10,7 @@ describe "Acceptance::Certificate" do
       expect(response.status).to eq(200)
     end
 
-    it "shows the EPC title" do
+    it "shows the non-domestic energy performance certificate title" do
       expect(response.body).to include(
         '<h1 class="govuk-heading-xl">Non-domestic Energy Performance Certificate</h1>',
       )
@@ -19,6 +19,18 @@ describe "Acceptance::Certificate" do
     it "shows the address summary" do
       expect(response.body).to include(
         '<p class="epc-address govuk-body">Flat 33<br>2 Marsham Street<br>London<br>SW1B 2BB</p>',
+      )
+    end
+
+    it "shows the energy rating title" do
+      expect(response.body).to include(
+        '<p class="epc-rating-title govuk-body">Energy Rating</p>',
+      )
+    end
+
+    it "shows the current energy energy efficiency band" do
+      expect(response.body).to include(
+        '<p class="epc-rating-result govuk-body">B</p>',
       )
     end
   end
