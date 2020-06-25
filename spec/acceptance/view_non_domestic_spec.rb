8a41ddd1c94f96de456bb0d3bf65753b38fd5d80
Yusuf <yusuf@madetech.com>
2020-06-25 14:59:02 +0100

    Refactor: Compact address details
    Co-authored-by: Christine <splogy2001@hotmail.com>

--- spec/acceptance/view_non_domestic_spec.rb
+++ spec/acceptance/view_non_domestic_spec.rb
@@ -18,8 +18,7 @@ describe "Acceptance::Certificate" do
 
     it "shows the address summary" do
       expect(response.body).to include(
-        '<p class="epc-address govuk-body">Flat 33<br>2 Marsham Street
-<br>London<br>SW1B 2BB</p>',
+        '<p class="epc-address govuk-body">Flat 33<br>2 Marsham Street<br>London<br>SW1B 2BB</p>',
       )
     end
   end
