4cb4295e5654a805152e891d28a35b19f6e57af7
Tito Sarrionandia <r@sarrionandia.com>
2020-08-02 21:34:32 +0100

    Show EPC summary box for DECs

--- spec/acceptance/view_dec_spec.rb
+++ spec/acceptance/view_dec_spec.rb
@@ -11,11 +11,20 @@ describe "Acceptance::DisplayEnergyCertificate" do
     before do
       FetchAssessmentSummary::AssessmentStub
           .fetch_dec("0000-0000-0000-0000-1111",
-                     "2030-02-29")
+                     "2030-02-21")
     end
 
     it "Shows the page title" do
       expect(response.body).to include("Display Energy Certificate")
+    end
+
+    it "Shows the summary box" do
+      expect(response.body).to include("0000-0000-0000-0000-1111")
+      expect(response.body).to include("Valid until 21 February 2030")
+      expect(response.body).to include("A")
+      expect(response.body).to include("2 Lonely Street")
+      expect(response.body).to include("Post-Town1")
+      expect(response.body).to include("A0 0AA")
     end
   end
 end
