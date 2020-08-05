14c355d1e5021608d3cd340908995a670072cda1
Tito Sarrionandia <r@sarrionandia.com>
2020-08-05 19:04:28 +0100

    Formatting

--- spec/acceptance/view_dec_spec.rb
+++ spec/acceptance/view_dec_spec.rb
@@ -33,11 +33,13 @@ describe "Acceptance::DisplayEnergyCertificate" do
         ">Energy performance of this building</h2>",
       )
       expect(response.body).to include(
-        'The building’s energy performance is based on its carbon dioxide (CO2) emissions for the last year.',
+        "The building’s energy performance is based on its carbon dioxide (CO2) emissions for the last year.",
       )
-      expect(response.body).to include('It is given a score and an energy rating on a scale from A (lowest emissions) to G (highest emissions).',)
       expect(response.body).to include(
-        'Typical energy performance for a public building is 100. This typical score gives an energy rating of D.',
+        "It is given a score and an energy rating on a scale from A (lowest emissions) to G (highest emissions).",
+      )
+      expect(response.body).to include(
+        "Typical energy performance for a public building is 100. This typical score gives an energy rating of D.",
       )
       expect(response.body).to include(">1 | A</text>")
       expect(response.body).to include(
