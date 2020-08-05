35d6d9eaae36043d52135ff8a6c78709442fec03
Yusuf <yusuf@madetech.com>
2020-08-05 10:52:39 +0100

    Add Rating section heading on DEC page
    Co-authored-by: Christine <splogy2001@hotmail.com>

--- spec/acceptance/view_dec_spec.rb
+++ spec/acceptance/view_dec_spec.rb
@@ -15,17 +15,21 @@ describe "Acceptance::DisplayEnergyCertificate" do
       )
     end
 
-    it "Shows the page title" do
+    it "shows the page title" do
       expect(response.body).to include("Display Energy Certificate")
     end
 
-    it "Shows the summary box" do
+    it "shows the summary box" do
       expect(response.body).to include("0000-0000-0000-0000-1111")
       expect(response.body).to include("Valid until 21 February 2030")
       expect(response.body).to include("A")
       expect(response.body).to include("2 Lonely Street")
       expect(response.body).to include("Post-Town1")
       expect(response.body).to include("A0 0AA")
+    end
+
+    it "shows the rating section" do
+      expect(response.body).to include(">Energy performance Operational Rating for this building</h2>")
     end
   end
 end
