d21130852cf50aa1e9aac31c1202d090d2d2e639
Yusuf <yusuf@madetech.com>
2020-08-05 12:28:21 +0100

    Update content on DEC page
    Co-authored-by: Christine <splogy2001@hotmail.com>

--- spec/acceptance/view_dec_spec.rb
+++ spec/acceptance/view_dec_spec.rb
@@ -33,16 +33,10 @@ describe "Acceptance::DisplayEnergyCertificate" do
         ">Energy performance of this building</h2>",
       )
       expect(response.body).to include(
-        '<p class="govuk-body">This tells you how efficiently energy has been used in the building. The numbers do not represent actual units of energy consumed; they represent comparative energy efficiency.</p>',
-      )
-      expect(response.body).to include(
-        '<p class="govuk-body">100 would be typical for this kind of building.</p>',
+        '<p class="govuk-body">The building’s energy performance is graded on a scale from A (most efficient) to G (least efficient).</p>',
       )
       expect(response.body).to include(">1 |</text>")
       expect(response.body).to include(">A</text>")
-      expect(response.body).to include(
-        '<p class="govuk-body">The operational rating is based on meter readings of all the energy actually used in the building including for lighting, heating, cooling, ventilation and hot water. It is compared to a benchmark that represents performance indicative of all buildings of this type.</p>',
-      )
       expect(response.body).to include(
         '<p class="govuk-body">You can read <a href="https://www.gov.uk/government/publications/display-energy-certificates-and-advisory-reports-for-public-buildings">guidance on Display Energy Certificates and advisory reports for public buildings</a>.</p>',
       )
