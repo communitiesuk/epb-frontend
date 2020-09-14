11df1c7ca793f5589286cf70e8602a68318f09a7
Tito Sarrionandia <r@sarrionandia.com>
2020-09-14 15:02:48 +0100

    Show sub systems section on ac report

--- spec/acceptance/view_ac_report_spec.rb
+++ spec/acceptance/view_ac_report_spec.rb
@@ -175,6 +175,13 @@ describe "Acceptance::AirConditioningInspectionReport", type: :feature do
                                           "VOL1/SYS1: Indoor wall type split which is part of a multi system with 5 indoor units."
     end
 
+    it "can show the system controls" do
+      expect(response.body).to have_css "h2", text: "System controls"
+      expect(response.body).to have_css "h3",
+                                        text:
+                                          "VOL1/SYS1: Indoor wall type split which is part of a multi system with 5 indoor units."
+    end
+
     context "when there are no subsystems" do
       before do
         FetchAssessmentSummary::AssessmentStub.fetch_ac_report(
