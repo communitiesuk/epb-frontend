5e41f2274acd9d51616083532d5bf77133136c9c
Tito Sarrionandia <r@sarrionandia.com>
2020-09-14 17:46:11 +0100

    Remove sudo from hosts file

--- scripts/configure-tests-hosts.sh
+++ scripts/configure-tests-hosts.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo echo "127.0.0.1       find-energy-certificate.local.gov.uk" >> /etc/hosts
-sudo echo "127.0.0.1       getting-new-energy-certificate.local.gov.uk" >> /etc/hosts
+echo "127.0.0.1       find-energy-certificate.local.gov.uk" >> /etc/hosts
+echo "127.0.0.1       getting-new-energy-certificate.local.gov.uk" >> /etc/hosts
