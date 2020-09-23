1842684f6a20e9f3fe5d8c256fa0bb2edf1b6478
Yusuf <yusuf@madetech.com>
2020-09-23 11:54:34 +0100

    Refactor: Use Capybara when testing domestic search by street name and town page

--- spec/acceptance/find_certificate_spec.rb
+++ spec/acceptance/find_certificate_spec.rb
@@ -367,41 +367,40 @@ describe "Acceptance::Certificate" do
     end
   end
 
-  describe ".get find-energy-certificate/find-a-certificate/search-by-street-name-and-town" do
+  describe ".get find-energy-certificate/find-a-certificate/search-by-street-name-and-town", type: :feature do
     context "when search page rendered" do
       let(:response) do
         get "http://find-energy-certificate.local.gov.uk/find-a-certificate/search-by-street-name-and-town"
       end
 
       it "returns status 200" do
-        expect(response.status).to eq(200)
+        expect(response.status).to eq 200
       end
 
       it "displays the find a certificate page heading" do
-        expect(response.body).to include(
-          "Find an energy performance certificate (EPC) by street and town",
-        )
+        expect(response.body).to have_css "h1", text: "Find an energy performance certificate (EPC) by street and town"
       end
 
       it "displays the data gap warning" do
-        expect(response.body).to include("Warning")
-        expect(response.body).to include(
-          "For certificates issued between 9 June 2020 and 19 September",
-        )
+        expect(response.body).to have_text "Warning"
+        expect(response.body).to have_css "strong", text: "For certificates issued between 9 June 2020 and 19 September 2020, and all expired certificates, you should use:"
+        expect(response.body).to have_css "li", text: "the EPC register for England and Wales"
+        expect(response.body).to have_css "li", text: "the EPC register for Northern Ireland"
+        expect(response.body).to have_css "strong", text: "Find all other valid certificates using this page’s search."
+        expect(response.body).to have_css "strong", text: "If you cannot find a certificate, you should contact the energy assessor who carried out the assessment."
       end
 
-      it "has an input field" do
-        expect(response.body).to include('<input id="town" name="town"')
+      it "has two input fields" do
+        expect(response.body).to have_css "input#street_name"
+        expect(response.body).to have_css "input#town"
       end
 
       it "has a Find button" do
-        expect(response.body).to include(
-          '<button class="govuk-button" data-module="govuk-button">Find</button>',
-        )
+        expect(response.body).to have_css "button", text: "Find"
       end
 
       it "does not display an error message" do
-        expect(response.body).not_to include("govuk-error-message")
+        expect(response.body).not_to have_text "govuk-error-message"
       end
     end
 
