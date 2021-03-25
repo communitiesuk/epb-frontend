34a0bcbf671518bcd71eb992262e2b69ce5726a3
Bella <bella.cockrell@madetech.com>
2021-03-25 15:42:20 +0000

    Adding certificates gateway test for dec summary

---
+++ spec/test_doubles/certificates_gateway/unsupported_schema_stub.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module CertificatesGateway
+  class UnsupportedSchemaStub
+    def self.fetch_dec_summary(assessment_id)
+
+      WebMock.stub_request(
+        :get,
+        "http://test-api.gov.uk/api/dec_summary/#{assessment_id}",
+      ).to_return(
+        status: 400,
+        body: {
+          "errors": [
+            {
+              "code": "INVALID_REQUEST",
+              "message": "Unsupported schema type",
+            },
+          ],
+        }.to_json,
+      )
+    end
+  end
+end
--- spec/unit/gateway/certificates_gateway_spec.rb
+++ spec/unit/gateway/certificates_gateway_spec.rb
@@ -6,58 +6,82 @@ describe Gateway::CertificatesGateway do
   let(:gateway) do
     described_class.new(container.get_object(:internal_api_client))
   end
-
-  context "when a certificate exist" do
-    let(:response) { gateway.search_by_postcode("SW1A 2AA", %w[RdSAP SAP]) }
-    let(:certificate) { response[:data][:assessments].first }
+  context "for the search by postcode gateway" do
+    context "when a certificate exists" do
+      let(:response) { gateway.search_by_postcode("SW1A 2AA", %w[RdSAP SAP]) }
+      let(:certificate) { response[:data][:assessments].first }
 
     before { FindCertificate::Stub.search_by_postcode("SW1A 2AA") }
 
-    it "checks the number of certificates returned from the api" do
-      expect(response[:data][:assessments].count).to eq(4)
+      it "checks the number of certificates returned from the api" do
+        expect(response[:data][:assessments].count).to eq(4)
+      end
+
+      it "checks the shape of the object passed in the certificate object" do
+        expect(certificate.keys).to contain_exactly(
+          :assessmentId,
+          :addressId,
+          :dateOfAssessment,
+          :dateRegistered,
+          :dwellingType,
+          :typeOfAssessment,
+          :totalFloorArea,
+          :currentCarbonEmission,
+          :potentialCarbonEmission,
+          :currentEnergyEfficiencyRating,
+          :currentEnergyEfficiencyBand,
+          :potentialEnergyEfficiencyRating,
+          :potentialEnergyEfficiencyBand,
+          :postcode,
+          :dateOfExpiry,
+          :heatDemand,
+          :recommendedImprovements,
+          :propertySummary,
+          :addressLine1,
+          :town,
+          :greenDealPlan,
+        )
+      end
     end
 
-    it "checks the shape of the object passed in the certificate object" do
-      expect(certificate.keys).to contain_exactly(
-        :assessmentId,
-        :addressId,
-        :dateOfAssessment,
-        :dateRegistered,
-        :dwellingType,
-        :typeOfAssessment,
-        :totalFloorArea,
-        :currentCarbonEmission,
-        :potentialCarbonEmission,
-        :currentEnergyEfficiencyRating,
-        :currentEnergyEfficiencyBand,
-        :potentialEnergyEfficiencyRating,
-        :potentialEnergyEfficiencyBand,
-        :postcode,
-        :dateOfExpiry,
-        :heatDemand,
-        :recommendedImprovements,
-        :propertySummary,
-        :addressLine1,
-        :town,
-        :greenDealPlan,
-      )
+    context "when a certificate doesnt exist" do
+      let(:response) { gateway.search_by_postcode("BF1 3AA", %w[RdSAP SAP]) }
+
+      before { FindCertificate::NoCertificatesStub.search_by_postcode }
+
+      it "returns empty results" do
+        expect(response).to eq(
+          data: {
+            assessments: [],
+          },
+          meta: {
+            searchPostcode: "BF1 3AA",
+          },
+        )
+      end
     end
   end
 
-  context "when a certificate doesnt exist" do
-    let(:response) { gateway.search_by_postcode("BF1 3AA", %w[RdSAP SAP]) }
+  context "for the fetch dec summary gateway" do
+    context "when a certificate is unsupported" do
+      let(:response) { gateway.fetch_dec_summary("0000-0000-0000-0000-1111") }
+      let(:error) { response[:status]}
 
-    before { FindCertificate::NoCertificatesStub.search_by_postcode }
+      before do
+        CertificatesGateway::UnsupportedSchemaStub.fetch_dec_summary("0000-0000-0000-0000-1111")
+      end
 
-    it "returns empty results" do
-      expect(response).to eq(
-        data: {
-          assessments: [],
-        },
-        meta: {
-          searchPostcode: "BF1 3AA",
-        },
-      )
+      it "returns not found error" do
+        expect(response).to eq(
+          "errors": [
+            {
+              "code": "INVALID_REQUEST",
+              "message": "Unsupported schema type",
+            },
+          ],
+        )
+      end
+
     end
   end
 end
