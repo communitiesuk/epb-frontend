9dc69b9a8c7333499e558bd1ebd7e4e4646fa963
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2021-05-14 12:14:17 +0100

    Don't break email address or phone number in the "other certificates" message

--- spec/acceptance/view_dec_spec.rb
+++ spec/acceptance/view_dec_spec.rb
@@ -260,7 +260,7 @@ describe "Acceptance::DisplayEnergyCertificate", type: :feature do
                                         text: "Other certificates for this property"
       expect(response.body).to have_css "p",
                                         text:
-                                          "If you are aware of previous certificates for this property and they are not listed here, please contact us at mhclg.digital-services@communities.gov.uk, or call our helpdesk on 020 3829 0748."
+                                          "If you are aware of previous certificates for this property and they are not listed here, please contact us at mhclg.digital-services@communities.gov.uk or call our helpdesk on 020 3829 0748."
       expect(response.body).to have_css "dt", text: "Certificate number"
       expect(response.body).to have_link "0000-0000-0000-0000-0001",
                                          href: "/energy-certificate/0000-0000-0000-0000-0001"
