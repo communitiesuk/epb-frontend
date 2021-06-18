6240ec53fed4e707d00858acee4e609221ea7a70
Douglas Greenshields <douglas.greenshields@communities.gov.uk>
2021-06-18 09:22:49 +0100

    demo

--- assets/javascript/cookie-consent.js
+++ assets/javascript/cookie-consent.js
@@ -1,9 +1,14 @@
+const dayInMilliseconds = 24*60*60*1000
+
+console.log('loading cookie consent script');
+console.log('cookies at script load: ' + document.cookie);
+
 const cookies = {
   ids: {
     'find-energy-certificate.digital.communities.gov.uk': 'G-ZDCS1W2ZRM',
     'getting-new-energy-certificate.digital.communities.gov.uk': 'G-TR7Y5Z1GFY',
     'getting-new-energy-certificate.local.gov.uk': 'G-TR7Y5Z1GFY',
-    'find-energy-certificate.local.gov.uk': 'G-ZDCS1W2ZRM'
+    'find-energy-certificate.epb-frontend': 'G-ZDCS1W2ZRM'
   },
 
   initialize: function () {
@@ -14,6 +19,8 @@ const cookies = {
     if (location.search.indexOf('cookies-setting=false') !== -1) return
 
     const cookieValue = cookies.read('cookie_consent')
+
+    console.log("cookie value is " + cookieValue);
 
     // Get Google Analytics going if consented
     if (cookieValue === 'true') {
@@ -96,6 +103,7 @@ const cookies = {
   },
 
   analytics: function (tagId) {
+    console.log('including analytics js');
     // Add Google Analytics script to page
     const dataScript = document.createElement('script')
     dataScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + tagId
@@ -112,7 +120,7 @@ const cookies = {
 
         gtag('js', new Date())
         gtag('config', tagId, {
-          'cookie_expires': 31536000
+          cookie_expires: 31536000,
         });
       },
       100
@@ -141,23 +149,94 @@ const cookies = {
   },
 
   erase: function () {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push(function() {
+      this.reset()
+    })
+
     const cookies = document.cookie.split('; ')
 
     for (let i = 0; i < cookies.length; i++) {
       if (cookies[i].startsWith('_ga')) {
-        const expDate = new Date()
+        let expDate = new Date()
+        expDate.setTime(expDate.getTime() - dayInMilliseconds)
         const name = cookies[i].split('=')[0] + '='
         const value = cookies[i].split('=')[1] + ';'
 
         const expires = ' expires=' + expDate.toUTCString() + ';'
         const path = ' path=/;'
-        const domain = ' domain=' + document.domain.split('.').slice(-3).join('.') + ';'
+        const domain = ' domain=.' + document.domain.split('.').slice(-3).join('.') + ';'
+
+        console.log(name + value + expires + path + domain);
 
         document.cookie = name + value + expires + path + domain
+
+        console.log(document.cookie);
       }
     }
-    document.cookie = 'cookie_consent=false;'
+    document.cookie = 'cookie_consent=false;path=/'
   }
 }
 
 window.onload = cookies.initialize
+
+const getState = () => {
+  if (document.visibilityState === 'hidden') {
+    return 'hidden';
+  }
+  if (document.hasFocus()) {
+    return 'active';
+  }
+  return 'passive';
+};
+
+// Stores the initial state using the `getState()` function (defined above).
+let state = getState();
+
+// Accepts a next state and, if there's been a state change, logs the
+// change to the console. It also updates the `state` value defined above.
+const logStateChange = (nextState) => {
+  const prevState = state;
+  if (nextState !== prevState) {
+    console.log(`State change: ${prevState} >>> ${nextState}`);
+    console.log(`Cookies at state change: ${document.cookie}`);
+    state = nextState;
+  }
+};
+
+// These lifecycle events can all use the same listener to observe state
+// changes (they call the `getState()` function to determine the next state).
+['pageshow', 'focus', 'blur', 'visibilitychange', 'resume'].forEach((type) => {
+  window.addEventListener(type, () => logStateChange(getState()), {capture: true});
+});
+
+// The next two listeners, on the other hand, can determine the next
+// state from the event itself.
+window.addEventListener('freeze', () => {
+  // In the freeze event, the next state is always frozen.
+  logStateChange('frozen');
+}, {capture: true});
+
+window.addEventListener('pagehide', (event) => {
+  if (event.persisted) {
+    // If the event's persisted property is `true` the page is about
+    // to enter the Back-Forward Cache, which is also in the frozen state.
+    logStateChange('frozen');
+  } else {
+    // If the event's persisted property is not `true` the page is
+    // about to be unloaded.
+    logStateChange('terminated');
+  }
+}, {capture: true});
+
+document.addEventListener('visibilitychange', function () {
+  console.log('document visibility changed to: ' + document.visibilityState);
+  console.log('cookies after visibility change: ' + document.cookie);
+  // if (document.visibilityState === 'hidden' && cookies.read('cookie_consent') === 'false') {
+  //   cookies.erase();
+  // }
+})
+
+document.addEventListener('resume', function () {
+  console.log('a resume happened')
+})
