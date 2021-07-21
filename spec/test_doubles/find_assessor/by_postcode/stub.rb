6f13d6e2f5e47d28f5e40aa62e0200e46ac2ca66
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2021-07-21 15:02:35 +0100

    Update test stubs for new Faraday version
    We're now at v1.5.0

--- spec/test_doubles/find_assessor/by_postcode/stub.rb
+++ spec/test_doubles/find_assessor/by_postcode/stub.rb
@@ -36,7 +36,7 @@ module FindAssessor
               :Accept => "*/*",
               "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
               :Authorization => "Bearer abc",
-              "User-Agent" => "Faraday v1.4.2",
+              "User-Agent" => "Faraday v1.5.0",
             },
           )
           .to_return(
