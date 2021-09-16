982bfe3db3139fb88ddd52f475fba071ce4f2e00
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2021-09-16 15:59:01 +0100

    Bump instance memory to 1.5G in production
    We have had increased traffic the last couple of days,
    frontend CPU has been maxed out and heathchecks failed.
    Adding memory also adds CPU capacity as CPU is allocated
    proportional with memory.

--- scripts/generate-paas-manifest.sh
+++ scripts/generate-paas-manifest.sh
@@ -5,7 +5,7 @@ APPLICATION_NAME=$1  # e.g. epb-something-api
 STAGE=$2 # i.e. [integration, staging, production]
 
 case "$STAGE" in
- production) MEMORY="1G" ;;
+ production) MEMORY="1536M" ;;
  *) MEMORY="256M" ;;
 esac
 
