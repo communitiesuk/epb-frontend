2cc7e1aaaf57c64accbd36f0d28e86eedcdbd5f5
Barry Halper <barryhalper@gmail.com>
2021-11-12 11:42:55 +0000

    EPBR-1400 Update erb with table from the design system and filled it with the data from the use case

--- spec/acceptance/view_service_performance_spec.rb
+++ spec/acceptance/view_service_performance_spec.rb
@@ -13,5 +13,28 @@ describe "Acceptance::ServicePerformance", type: :feature do
     it "has the correct title" do
       expect(response.body).to include("<title>Service Performance</title>")
     end
+
+    it "has the correct H1" do
+      expect(response.body).to have_css("h1", text: "Service Performance")
+    end
+
+    it "has a table for the data set" do
+      expect(response.body).to have_table
+      expect(response.body).to have_selector(".govuk-table")
+      expect(response.body).to have_selector("caption", text: "Monthly assessments stat")
+    end
+
+    it "the table has the correct number of rows : n th(1) + n stub(17)" do
+      expect(response.body).to have_css("table.govuk-table tr", count: 18)
+    end
+
+    it "the table writes the body into all the relevant columns" do
+      ServicePerformance::Stub.body[:data].each do |row|
+        expect(response.body).to have_css("table.govuk-table tr>td.month-year", text: row[:monthYear].to_s)
+        expect(response.body).to have_css("table.govuk-table tr>td.assessment-type", text: row[:assessmentType].to_s)
+        expect(response.body).to have_css("table.govuk-table tr>td.num-assessments", text: row[:numAssessments].to_s)
+        expect(response.body).to have_css("table.govuk-table tr>td.rating-average", text: row[:ratingAverage].to_s)
+      end
+    end
   end
 end
