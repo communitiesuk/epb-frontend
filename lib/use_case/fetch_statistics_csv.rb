db82597c269bdd513a6b1226c4357400e6bab65b
Barry Halper <barryhalper@gmail.com>
2021-11-17 10:05:24 +0000

    EPBR-1714 Added use case to extract data and reformat

--- lib/use_case/fetch_statistics_csv.rb
+++ lib/use_case/fetch_statistics_csv.rb
@@ -4,8 +4,21 @@ module UseCase
   class FetchStatisticsCsv < UseCase::Base
     def execute
       results = @gateway.fetch
-      results[:data].group_by { |h| h[:month] }
-
+      return_array = []
+      months = results[:data].group_by { |h| h[:month] }.keys
+      types = results[:data].group_by { |h| h[:assessmentType] }.keys
+      months.each do |month|
+        hash = { "Month" => Date.parse("#{month}-01").strftime("%b-%Y") }
+        types.each do |type|
+          stats_item = results[:data].select { |item| item[:month] == month && item[:assessmentType] == type }.first
+          hash["#{type}s Lodged"] = stats_item[:numAssessments]
+          if %w[SAP RdSAP CEPC].include?(type)
+            hash["Average #{type} Energy Rating"] = stats_item[:ratingAverage]
+          end
+        end
+        return_array << hash
+      end
+      return_array
     end
   end
 end
--- spec/unit/use_case/fetch_statistics_csv_spec.rb
+++ spec/unit/use_case/fetch_statistics_csv_spec.rb
@@ -2,44 +2,122 @@ describe UseCase::FetchStatisticsCsv do
   include RSpecUnitMixin
 
   context "when converting statistics data from the API to format for the csv" do
-    let(:statistics_gateway) { Gateway::StatisticsGateway.new(get_api_client) }
+    let(:statistics_gateway) { instance_double(Gateway::StatisticsGateway) }
+    let(:api_data) do
+      { data: [
+        {
+          "numAssessments": 21_163,
+          "assessmentType": "SAP",
+          "ratingAverage": 78.41,
+          "month": "2021-10",
+        },
+        {
+          "numAssessments": 122_394,
+          "assessmentType": "RdSAP",
+          "ratingAverage": 61.71,
+          "month": "2021-10",
+        },
+        {
+          "numAssessments": 8208,
+          "assessmentType": "CEPC",
+          "ratingAverage": 67.36,
+          "month": "2021-10",
+        },
+        {
+          "numAssessments": 2189,
+          "assessmentType": "DEC",
+          "ratingAverage": 0.0,
+          "month": "2021-10",
+        },
+
+        {
+          "numAssessments": 470,
+          "assessmentType": "DEC-RR",
+          "ratingAverage": 0.0,
+          "month": "2021-10",
+        },
+
+        {
+          "numAssessments": 1251,
+          "assessmentType": "AC-CERT",
+          "ratingAverage": 0.0,
+          "month": "2021-10",
+        },
+
+        {
+          "numAssessments": 24_477,
+          "assessmentType": "SAP",
+          "ratingAverage": 77.83,
+          "month": "2021-09",
+        },
+        {
+          "numAssessments": 121_499,
+          "assessmentType": "RdSAP",
+          "ratingAverage": 61.71,
+          "month": "2021-09",
+        },
+        {
+          "numAssessments": 7718,
+          "assessmentType": "CEPC",
+          "ratingAverage": 68.27,
+          "month": "2021-09",
+        },
+        {
+          "numAssessments": 3500,
+          "assessmentType": "DEC",
+          "ratingAverage": 67.36,
+          "month": "2021-09",
+        },
+
+        {
+          "numAssessments": 428,
+          "assessmentType": "DEC-RR",
+          "ratingAverage": 0.0,
+          "month": "2021-09",
+        },
+
+        {
+          "numAssessments": 918,
+          "assessmentType": "AC-CERT",
+          "ratingAverage": 0.0,
+          "month": "2021-09",
+        },
+      ] }
+    end
+
     let(:expectation) do
-      [{ "Month" => "Oct-21",
+      [{ "Month" => "Oct-2021",
          "SAPs Lodged" => 21_163,
          "Average SAP Energy Rating" => 78.41,
          "RdSAPs Lodged" => 122_394,
          "Average RdSAP Energy Rating" => 61.71,
          "CEPCs Lodged" => 8208,
          "Average CEPC Energy Rating" => 67.36,
-         "DECs Lodged" => 2819,
-         "Average DEC Energy Rating" => nil,
-         "DEC-RRs Lodged" => 125,
-         "Average DEC-RR Energy Rating" => nil,
-         "AC-Reports Lodged" => 470,
-         "Average AC-Report Energy Rating" => nil },
-       { "Month" => "Sept-21",
+         "DECs Lodged" => 2189,
+         "DEC-RRs Lodged" => 470,
+         "AC-CERTs Lodged" => 1251 },
+       { "Month" => "Sep-2021",
          "SAPs Lodged" => 24_477,
          "Average SAP Energy Rating" => 77.83,
          "RdSAPs Lodged" => 121_499,
          "Average RdSAP Energy Rating" => 61.71,
          "CEPCs Lodged" => 7718,
-         "Average CEPC Energy Rating" => 67.36,
+         "Average CEPC Energy Rating" => 68.27,
          "DECs Lodged" => 3500,
-         "Average DEC Energy Rating" => nil,
          "DEC-RRs Lodged" => 428,
-         "Average DEC-RR Energy Rating" => nil,
-         "AC-Reports Lodged" => 918,
-         "Average AC-Report Energy Rating" => nil }]
+         "AC-CERTs Lodged" => 918 }]
     end
     let(:fetch_statistics) { described_class.new(statistics_gateway) }
     let(:results) { fetch_statistics.execute }
 
     before do
-      ServicePerformance::Stub.statistics
+      # ServicePerformance::Stub.statistics
+      # allow(statistics_gateway).to receive(:new).and_return(get_api_client)
+      allow(statistics_gateway).to receive(:fetch).and_return(api_data)
     end
 
     it "groups the data by months " do
-      expect(results.keys).to eq(["11-2020", "09-2021", "10-2020", "11-2021", "03-2021", "02-2021", "07-2021", "06-2021", "01-2021", "12-2020", "04-2021"])
+      expect(results).to eq(expectation)
     end
   end
 end
