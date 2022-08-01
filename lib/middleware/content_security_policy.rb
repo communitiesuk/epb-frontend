79850bc977ac6a92458adb5447ac0670ff45baab
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2022-08-01 16:18:41 +0100

    add some documentation for custom CSP middleware

--- lib/middleware/content_security_policy.rb
+++ lib/middleware/content_security_policy.rb
@@ -1,6 +1,17 @@
 require "rack/protection/content_security_policy"
 
 module Middleware
+  ##
+  # A custom Rack middleware that wraps the ContentSecurityPolicy middleware from the Rack::Protection module and adds:
+  #
+  # 1. the ability to switch on enforcement of a content security policy within browsers (using the Content-Security-Policy
+  # header rather than Content-Security-Policy-Report-Only) using the feature flag "frontend-enforce-content-security-policy"
+  # 2. the additional option `report_ratio`, which takes a number of between 0 and 1. This makes the CSP policy only include
+  # a report-uri directive in the given ratio of request/response cycles - i.e. a ratio of 0.1 should only send a report-uri
+  # on average once every ten responses. This is in order to limit the number of reports that are sent to Sentry (as long-tail
+  # cases are not of interest as they represent the CSP doing its job, and could use up Sentry quotas unnecessarily - we'd want
+  # to know, though, if the CSP were blocking anything relating to the function of Google Analytics, for example, and would
+  # expect these to appear in sufficiently large frequencies to be visible).
   class ContentSecurityPolicy
     def initialize(app, options = {})
       @csp_middleware = Rack::Protection::ContentSecurityPolicy.new(app, options.reject { |k, _| k == :report_ratio })
