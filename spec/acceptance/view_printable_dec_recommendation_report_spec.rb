acc57ad18e884dce57f70f3088416aaa29aa7da6
Christine Horrocks <christine.horrocks@levellingup.gov.uk>
2022-11-24 13:39:09 +0000

    Format changes for printable DEC RR

--- spec/acceptance/view_printable_dec_recommendation_report_spec.rb
+++ spec/acceptance/view_printable_dec_recommendation_report_spec.rb
@@ -1,14 +1,13 @@
 describe "Acceptance::NonDomesticEnergyPerformanceCertificateRecommendationReportPrintView", type: :feature do
   include RSpecFrontendServiceMixin
 
-
   context "when the report is superseded" do
     before do
       FetchAssessmentSummary::AssessmentStub.fetch_dec_rr(
         assessment_id: "1234-5678-1234-5678-1234",
         date_of_expiry: "2020-01-01",
         superseded_by: "1234-5678-1234-5678-1235",
-        )
+      )
     end
 
     let(:print_response) { get "/energy-certificate/1234-5678-1234-5678-1234?print=true" }
