659483c1de87178ac5a5e5d57954066eddf611b5
Christine Horrocks <christine.horrocks@levellingup.gov.uk>
2022-11-24 13:34:45 +0000

    Add superseded warning to printable DEC RR

--- spec/acceptance/view_printable_dec_recommendation_report_spec.rb
+++ spec/acceptance/view_printable_dec_recommendation_report_spec.rb
@@ -1,6 +1,27 @@
 describe "Acceptance::NonDomesticEnergyPerformanceCertificateRecommendationReportPrintView", type: :feature do
   include RSpecFrontendServiceMixin
 
+
+  context "when the report is superseded" do
+    before do
+      FetchAssessmentSummary::AssessmentStub.fetch_dec_rr(
+        assessment_id: "1234-5678-1234-5678-1234",
+        date_of_expiry: "2020-01-01",
+        superseded_by: "1234-5678-1234-5678-1235",
+        )
+    end
+
+    let(:print_response) { get "/energy-certificate/1234-5678-1234-5678-1234?print=true" }
+
+    it "shows an superseded warning message" do
+      expect(print_response.body).to have_css(".govuk-warning-text", text: "A new report has replaced this one")
+    end
+
+    it "does not show warning text for expiry" do
+      expect(print_response.body).not_to have_css(".govuk-warning-text", text: "This report has expired")
+    end
+  end
+
   context "when the report is expired" do
     before do
       FetchAssessmentSummary::AssessmentStub.fetch_dec_rr(
