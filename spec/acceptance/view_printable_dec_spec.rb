957c24fe382ff6a1c578a8c24e1bd113e062e5d8
Christine Horrocks <christine.horrocks@levellingup.gov.uk>
2022-11-28 16:19:00 +0000

    Add expired to printable DEC

--- spec/acceptance/view_printable_dec_spec.rb
+++ spec/acceptance/view_printable_dec_spec.rb
@@ -164,6 +164,26 @@ describe "Acceptance::PrintableDisplayEnergyCertificate", type: :feature do
         expect(print_response.body).not_to have_css(".govuk-warning-text", text: "This certificate has expired")
       end
     end
+
+    context "when the report is expired" do
+      before do
+        FetchAssessmentSummary::AssessmentStub.fetch_dec(
+          assessment_id: "1234-5678-1234-5678-1234",
+          date_of_expiry: "2020-01-01",
+          superseded_by: nil,
+          )
+      end
+
+      let(:print_response) { get "/energy-certificate/1234-5678-1234-5678-1234?print=true" }
+
+      it "shows an superseded warning message" do
+        expect(print_response.body).not_to have_css(".govuk-warning-text", text: "A new certificate has replaced this one")
+      end
+
+      it "does not show warning text for expiry" do
+        expect(print_response.body).to have_css(".govuk-warning-text", text: "This certificate has expired")
+      end
+    end
   end
 
   context "when a printable dec certificate is both NI and opted out" do
