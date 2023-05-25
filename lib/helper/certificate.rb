1c407025a92a70205987bc61de42fae39c5bee26
Douglas Greenshields <douglas.greenshields@levellingup.gov.uk>
2023-05-25 16:56:49 +0100

    EPBR-3295 feat: moved flow control of hide_bills_text to external function

---
+++ lib/helper/certificate.rb
@@ -0,0 +1,26 @@
+module Helper
+  class Certificate
+    def self.hide_bills_text?(assessment)
+      assessment[:heatingCostCurrent]&.chomp(".00") == "0" ||
+        assessment[:heatingCostPotential]&.chomp(".00") == "0" ||
+        assessment[:hotWaterCostCurrent]&.chomp(".00") == "0" ||
+        assessment[:hotWaterCostPotential]&.chomp(".00") == "0" ||
+        assessment[:lightingCostCurrent]&.chomp(".00") == "0" ||
+        assessment[:lightingCostPotential]&.chomp(".00") == "0" ||
+        assessment[:estimatedEnergyCost].nil? ||
+        assessment[:potentialEnergySaving].nil?
+    end
+
+    def self.hide_heating_demand?(assessment)
+      return true if assessment[:heatDemand].nil?
+
+      (assessment[:heatDemand][:currentWaterHeatingDemand].nil? && assessment[:heatDemand][:currentSpaceHeatingDemand].nil?)
+    end
+
+    def self.hide_insulation_impact?(assessment)
+      return true if assessment[:heatDemand].nil?
+
+      (assessment[:heatDemand][:impactOfLoftInsulation].nil? && assessment[:heatDemand][:impactOfCavityInsulation].nil? && assessment[:heatDemand][:impactOfSolidWallInsulation].nil?)
+    end
+  end
+end
---
+++ spec/unit/helper/certificate_spec.rb
@@ -0,0 +1,100 @@
+describe Helper::Certificate do
+  let(:helper) { described_class }
+
+  describe "#hide_bills_text?" do
+    context "when all the attributes are null or empty" do
+      let(:assessment) do
+        {
+          heatingCostCurrent: "0",
+          heatingCostPotential: "0",
+          lightingCostCurrent: "0",
+          estimatedEnergyCost: "0",
+          potentialEnergySaving: nil,
+        }
+      end
+
+      it "returns true" do
+        expect(helper.hide_bills_text?(assessment)).to eq true
+        assessment[:heatingCostCurrent] = nil
+        assessment[:estimatedEnergyCost] = nil
+        expect(helper.hide_bills_text?(assessment)).to eq true
+      end
+    end
+
+    context "when the attributes has positive values" do
+      let(:assessment) do
+        {
+          heatingCostCurrent: "1",
+          heatingCostPotential: "1",
+          lightingCostCurrent: "1",
+          estimatedEnergyCost: "1",
+          potentialEnergySaving: "2",
+        }
+      end
+
+      it "returns false" do
+        expect(helper.hide_bills_text?(assessment)).to eq false
+      end
+    end
+  end
+
+  describe "#hide_heating_demand?" do
+    context "when all the attributes are null or empty" do
+      let(:assessment) do
+        {
+          heatDemand: { currentWaterHeatingDemand: nil, currentSpaceHeatingDemand: nil  },
+        }
+      end
+
+      it "returns false" do
+        expect(helper.hide_heating_demand?(assessment)).to eq true
+        assessment[:currentSpaceHeatingDemand] = "5"
+        expect(helper.hide_heating_demand?(assessment)).to eq true
+        assessment[:heatDemand] = nil
+        expect(helper.hide_heating_demand?(assessment)).to eq true
+      end
+    end
+
+    context "when the attributes have positive values" do
+      let(:assessment) do
+        {
+          heatDemand: { currentWaterHeatingDemand: "1", currentSpaceHeatingDemand: "4"  },
+        }
+      end
+
+      it "returns false" do
+        expect(helper.hide_heating_demand?(assessment)).to eq false
+      end
+    end
+  end
+
+  describe "#hide_insulation_impact?" do
+    context "when all the attributes are null or empty" do
+      let(:assessment) do
+        {
+          heatDemand: { impactOfLoftInsulation: nil, impactOfCavityInsulation: nil, impactOfSolidWallInsulation: nil  },
+        }
+      end
+
+      it "returns false" do
+        expect(helper.hide_insulation_impact?(assessment)).to eq true
+        assessment[:impactOfLoftInsulation] = "5"
+        expect(helper.hide_heating_demand?(assessment)).to eq true
+        assessment[:heatDemand] = nil
+        expect(helper.hide_insulation_impact?(assessment)).to eq true
+      end
+    end
+
+    context "when the attributes have positive values" do
+      let(:assessment) do
+        {
+          heatDemand: { impactOfLoftInsulation: "1", impactOfCavityInsulation: "6", impactOfSolidWallInsulation: "4"  },
+        }
+      end
+
+      it "returns false" do
+        expect(helper.hide_insulation_impact?(assessment)).to eq false
+      end
+    end
+  end
+end
