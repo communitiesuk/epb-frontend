54d782e58283cfbe40438abcbe9998ae0ac4a7eb
Eleanor Mollett <eleanor.mollett@gmail.com>
2023-06-02 13:25:16 +0100

    EPBR-3267 Use Clipboard API where possible when copying certificate link

--- assets/javascript/share-certificate.js
+++ assets/javascript/share-certificate.js
@@ -8,7 +8,15 @@ function copyToClipboard () {
   const input = document.body.appendChild(document.createElement('input'))
   input.value = window.location.href
   input.select()
-  document.execCommand('copy')
+
+  navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
+    if (result.state === 'granted' || result.state === 'prompt') {
+      navigator.clipboard.writeText(input.value)
+    } else {
+      document.execCommand('copy')
+    }
+  })
+
   input.parentNode.removeChild(input)
 }
 
