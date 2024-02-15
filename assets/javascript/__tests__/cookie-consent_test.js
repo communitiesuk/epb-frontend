556ec9528d2211091a26225e2b472a546ce0551b
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-02-15 09:18:37 +0000

    EPBR-4773 fixed reject cookie test

--- assets/javascript/__tests__/cookie-consent_test.js
+++ assets/javascript/__tests__/cookie-consent_test.js
@@ -54,34 +54,6 @@ describe('using GTM (Google Tag Manager)', () => {
     })
   })
 
-  describe('when calling the reject cookie function to remove _ga* cookies  ', () => {
-    beforeEach(() => {
-
-      Object.defineProperty(window.document, 'cookie', {
-        writable: true,
-        value: '_ga=12345; ga_123458=458489489'
-      })
-
-      Object.defineProperty(window.document, 'domain', {
-        writable: true,
-        value: '.find-energy-certificate.service.gov.uk'
-      })
-      init()
-      const gtag = (...args) => window.dataLayer.push(args)
-      const objCookie = cookieConsent(window.GOOGLE_PROPERTY, window, gtag)
-      objCookie.rejectCookieConsent()
-    })
-
-    it('removes all cookies that starts with _ga', () => {
-      expect(window.document.cookie).not.toContain('_ga')
-    })
-
-    it('set the cookie_consent cookie to false', () => {
-      expect(window.document.cookie).toContain('cookie_consent=false')
-    })
-
-  })
-
 
   describe('when cookies have not been accepted or rejected, but are accepted from user clicking acceptance', () => {
     beforeEach(() => {
@@ -96,6 +68,38 @@ describe('using GTM (Google Tag Manager)', () => {
       )
     })
   })
+
+})
+
+describe('when calling the reject cookie function to remove _ga* cookies  ', () => {
+  beforeEach(() => {
+
+    Object.defineProperty(window.document, 'cookie', {
+      writable: true,
+      value: '_ga=12345; ga_123458=458489489'
+    })
+
+    Object.defineProperty(window.document, 'domain', {
+      writable: true,
+      value: '.find-energy-certificate.service.gov.uk'
+    })
+    init()
+    const gtag = (...args) => window.dataLayer.push(args)
+    const objCookie = cookieConsent(window.GOOGLE_PROPERTY, window, gtag)
+    objCookie.rejectCookieConsent()
+  })
+  afterEach(() => {
+    window.dataLayer = []
+  })
+
+  it('removes all cookies that starts with _ga', () => {
+    expect(window.document.cookie).not.toContain('_ga')
+  })
+
+  it('set the cookie_consent cookie to false', () => {
+    expect(window.document.cookie).toContain('cookie_consent=false')
+  })
+
 })
 
 function callScript () {
