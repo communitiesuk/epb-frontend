90d07b39b210ac1934de1b7e060c49e9eea22572
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2024-08-15 15:15:52 +0100

    EPBR-6353 fixes the integration-restart-status codebuild
    Correctly passes the assumed role profile to the check_deployment_status.sh when executed in the code build stage

--- scripts/check_deployment_status.sh
+++ scripts/check_deployment_status.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-CLUSTER_NAME=$1
-SERVICE_NAME=$2
+PROFILE=$1
+CLUSTER_NAME=$2
+SERVICE_NAME=$3
 
 
 #
-STATUS=$(aws ecs describe-services --services $SERVICE_NAME --cluster $CLUSTER_NAME | jq -rc '.services[0].deployments[0].rolloutState' )
+STATUS=$(aws ecs describe-services --services $SERVICE_NAME --cluster $CLUSTER_NAME  --profile $PROFILE | jq -rc '.services[0].deployments[0].rolloutState' )
 ##echo "${STATUS}"
 ##if [[ $STATUS == "COMPLETED" ]]; then
 ##  echo "done"
@@ -13,7 +14,7 @@ STATUS=$(aws ecs describe-services --services $SERVICE_NAME --cluster $CLUSTER_N
 #exit 0
 
 while [[ $STATUS == "IN_PROGRESS" ]]; do
-STATUS=$(aws ecs describe-services --services $SERVICE_NAME --cluster $CLUSTER_NAME | jq -rc '.services[0].deployments[0].rolloutState' )
+STATUS=$(aws ecs describe-services --services $SERVICE_NAME --cluster $CLUSTER_NAME --profile $PROFILE | jq -rc '.services[0].deployments[0].rolloutState' )
 echo "${STATUS} << WAITING FOR RESTART STATUS TO REACH COMPLETED"
 sleep 20
 done
