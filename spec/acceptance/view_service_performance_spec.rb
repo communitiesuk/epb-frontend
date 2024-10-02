6bcb9b490930098d46e18e11af1bb3f096301e01
christinehorrocks <christine.horrocks@communities.gov.uk>
2024-10-02 15:58:11 +0100

    EPBR-7017 Add co2 bullet point for service performance

--- spec/acceptance/view_service_performance_spec.rb
+++ spec/acceptance/view_service_performance_spec.rb
@@ -24,6 +24,7 @@ describe "Acceptance::ServicePerformance", type: :feature do
         expect(response.body).to have_css("div", text: "Use this page to find data on:")
         expect(response.body).to have_css("ul.govuk-list li", text: "the number of energy certificates uploaded to the Energy Performance of Buildings Register")
         expect(response.body).to have_css("ul.govuk-list li", text: "the average energy rating for domestic and non-domestic properties")
+        expect(response.body).to have_css("ul.govuk-list li", text: "the average CO2 emissions per square metre for domestic properties")
         expect(response.body).to have_css("div", text: "This data covers England and Wales, and Northern Ireland.")
         expect(response.body).to have_css("div", text: "Updated: monthly")
       end
