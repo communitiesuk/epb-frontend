b414ed4c1b9b7c7afdca3d11a82b3758f62780d9
Eleanor Mollett <eleanor.mollett@gmail.com>
2025-05-20 15:04:44 +0100

    Add the Capybara options from Kev's branch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -159,13 +159,16 @@ Capybara.register_driver :custom_chrome_headless do |app|
   version = Capybara::Selenium::Driver.load_selenium
   options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
   browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-    opts.add_argument('--headless=new')
-    opts.add_argument('--disable-gpu') if Gem.win_platform?
+    opts.add_argument("--headless=new")
+    opts.add_argument("--disable-gpu") if Gem.win_platform?
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
-    opts.add_argument('--disable-site-isolation-trials')
-    opts.add_argument('--disable-background-timer-throttling')
-    opts.add_argument('--disable-backgrounding-occluded-windows')
-    opts.add_argument('--disable-renderer-backgrounding')
+    opts.add_argument("--disable-site-isolation-trials")
+    opts.add_argument("--disable-background-timer-throttling")
+    opts.add_argument("--disable-backgrounding-occluded-windows")
+    opts.add_argument("--disable-infobars")
+    opts.add_argument("--disable-renderer-backgrounding")
+    opts.add_argument("--no-sandbox")
+    opts.add_argument("--window-size=1280,1024")
   end
 
   Capybara::Selenium::Driver.new(app, **{ :browser => :chrome, options_key => browser_options })
