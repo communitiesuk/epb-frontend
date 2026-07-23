5c228f5e71b903764a8a3ce77eb08c1e41d21eee
Daniel Lewis <daniel.lewis@communities.gov.uk>
2026-07-23 09:48:00 +0100

    EPBR-9492, EPBR-6082: display ac cert subsystems as a dl
    The ac cert subsystems are a table that is too wide on a mobile
    
    The similar (but larger) ac report substypes are displayed as a dl
    
    Convert ac cert subsystems to a dl to match ac report.  Also fixes a html
    injection attack.

--- spec/acceptance/view_ac_cert_spec.rb
+++ spec/acceptance/view_ac_cert_spec.rb
@@ -60,13 +60,10 @@ describe "Acceptance::AirConditioningInspectionCertificate", type: :feature do
       expect(response.body).to have_css "dt", text: "Assessment software"
       expect(response.body).to have_css "dd", text: "CLG, ACReport, v2.0"
       expect(response.body).to have_css "dt", text: "Assessor’s declaration"
-      expect(response.body).to have_css "dd",
-                                        text:
-                                          "Not related to the owner/occupier or person who has technical control of the system or subcontractor."
+      expect(response.body).to have_css "dd", text: "Not related to the owner/occupier or person who has technical control of the system or subcontractor."
       expect(response.body).to have_css "dt", text: "F-Gas compliant date"
       expect(response.body).to have_css "dd", text: "20 September 2010"
-      expect(response.body).to have_css "dt",
-                                        text: "Total effective rated output"
+      expect(response.body).to have_css "dt", text: "Total effective rated output"
       expect(response.body).to have_css "dd", text: "106 kW"
       expect(response.body).to have_css "dt", text: "System sampling"
       expect(response.body).to have_css "dd", text: "Yes"
@@ -74,25 +71,19 @@ describe "Acceptance::AirConditioningInspectionCertificate", type: :feature do
       expect(response.body).to have_css "dd", text: "410 square metres"
       expect(response.body).to have_css "dt", text: "Subsystems metered"
       expect(response.body).to have_css "dd", text: "No"
-      expect(response.body).to have_css "dt",
-                                        text: "Total estimated refrigerant charge"
+      expect(response.body).to have_css "dt", text: "Total estimated refrigerant charge"
       expect(response.body).to have_css "dd", text: "73 kg"
     end
 
     it "shows the subsystems inspected section" do
       expect(response.body).to have_css "h2", text: "Subsystems inspected"
-      expect(response.body).to have_css "th", text: "Subsystem ID"
-      expect(response.body).to have_css "th", text: "Description"
-      expect(response.body).to have_css "th", text: "Refrigerant type"
-      expect(response.body).to have_css "th", text: "Age of main components"
-      expect(response.body).to have_css "td",
-                                        text:
-                                          "VOL001/SYS001/CP001 Ground Floor Banking Hall, Interview Room and Cashiers Area"
-      expect(response.body).to have_css "td",
-                                        text:
-                                          "Mitsubishi Electric PURY-P250LM-A1 VRF packaged system x 1 serve ceiling slot diffuser internal unit x 6"
-      expect(response.body).to have_css "td", text: "R410A"
-      expect(response.body).to have_css "td", text: "2013"
+      expect(response.body).to have_css "h3", text: "VOL001/SYS001/CP001 Ground Floor Banking Hall, Interview Room and Cashiers Area"
+      expect(response.body).to have_css "dt", text: "Description"
+      expect(response.body).to have_css "dd", text: "Mitsubishi Electric PURY-P250LM-A1 VRF packaged system x 1 serve ceiling slot diffuser internal unit x 6"
+      expect(response.body).to have_css "dt", text: "Refrigerant type"
+      expect(response.body).to have_css "dd", text: "R410A"
+      expect(response.body).to have_css "dt", text: "Age of main components"
+      expect(response.body).to have_css "dd", text: "2013"
     end
 
     it "can show the Assessor's details section" do
@@ -103,16 +94,14 @@ describe "Acceptance::AirConditioningInspectionCertificate", type: :feature do
       expect(response.body).to have_css "dd", text: "SPEC000000"
       expect(response.body).to have_css "dt", text: "Accreditation scheme"
       expect(response.body).to have_css "dd", text: "Quidos"
-      expect(response.body).to have_css "dt",
-                                        text: "Accreditation scheme telephone"
+      expect(response.body).to have_css "dt", text: "Accreditation scheme telephone"
       expect(response.body).to have_css "dd", text: "01225 667 570"
       expect(response.body).to have_css "dt", text: "Accreditation scheme email"
       expect(response.body).to have_css "dd", text: "info@quidos.co.uk"
       expect(response.body).to have_css "dt", text: "Employer/Trading name"
       expect(response.body).to have_css "dd", text: "Joe Bloggs Ltd"
       expect(response.body).to have_css "dt", text: "Employer/Trading address"
-      expect(response.body).to have_css "dd",
-                                        text: "123 My Street, My City, AB3 4CD"
+      expect(response.body).to have_css "dd", text: "123 My Street, My City, AB3 4CD"
     end
 
     context "with different related party disclosure codes" do
@@ -278,14 +267,11 @@ describe "Acceptance::AirConditioningInspectionCertificate", type: :feature do
       expect(response.body).not_to have_css "dd", text: "SPEC000000"
       expect(response.body).not_to have_css "dt", text: "Employer/Trading name"
       expect(response.body).not_to have_css "dd", text: "Joe Bloggs Ltd"
-      expect(response.body).not_to have_css "dt",
-                                            text: "Employer/Trading address"
-      expect(response.body).not_to have_css "dd",
-                                            text: "123 My Street, My City, AB3 4CD"
+      expect(response.body).not_to have_css "dt", text: "Employer/Trading address"
+      expect(response.body).not_to have_css "dd", text: "123 My Street, My City, AB3 4CD"
       expect(response.body).to have_css "dt", text: "Accreditation scheme"
       expect(response.body).to have_css "dd", text: "Quidos"
-      expect(response.body).to have_css "dt",
-                                        text: "Accreditation scheme telephone"
+      expect(response.body).to have_css "dt", text: "Accreditation scheme telephone"
       expect(response.body).to have_css "dd", text: "01225 667 570"
       expect(response.body).to have_css "dt", text: "Accreditation scheme email"
       expect(response.body).to have_css "dd", text: "info@quidos.co.uk"
